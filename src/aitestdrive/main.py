0ba5aa0854d092bdeaa9fcdf88683c5d7afac376
etvt <149910696+etvt@users.noreply.github.com>
2023-11-24 15:59:46 +0100

    Accept $PORT env var.

--- src/aitestdrive/main.py
+++ src/aitestdrive/main.py
@@ -1,8 +1,11 @@
+import os
+
 import uvicorn
 
 
 def main(**kwargs):
-    uvicorn.run("aitestdrive.controller.app:api", host="0.0.0.0", port=8000, **kwargs)
+    port = int(os.environ.get('PORT', 8000))  # Google Cloud sends us the $PORT it wants us to listen on
+    uvicorn.run("aitestdrive.controller.app:api", host="0.0.0.0", port=port, **kwargs)
 
 
 def main_dev():
