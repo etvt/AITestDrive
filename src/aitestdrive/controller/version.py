1b3bb89687784ad83c1ebc36c31dfffdce211366
etvt <149910696+etvt@users.noreply.github.com>
2023-11-28 14:39:55 +0100

    Better version response format.

--- src/aitestdrive/controller/version.py
+++ src/aitestdrive/controller/version.py
@@ -2,20 +2,26 @@ import logging
 import os
 
 from fastapi import APIRouter
+from pydantic import BaseModel
 
 log = logging.getLogger(__name__)
 
 api = APIRouter(prefix="/version", tags=["Version info"])
 
 
+class ServiceInfo(BaseModel):
+    service_name: str
+    service_revision: str
+
+
 @api.get("")
-async def chat() -> str:
+async def chat() -> ServiceInfo:
     log.debug(f"Version request received.")
 
-    service_name = os.environ.get('K_SERVICE')
-    service_revision = os.environ.get('K_REVISION')
+    service_name = os.environ.get('K_SERVICE', 'Unknown')
+    service_revision = os.environ.get('K_REVISION', 'Unknown')
 
-    return f"""Running!
-    Service name: {service_name}
-    Service revision: {service_revision}
-"""
+    return ServiceInfo(
+        service_name=service_name,
+        service_revision=service_revision
+    )
